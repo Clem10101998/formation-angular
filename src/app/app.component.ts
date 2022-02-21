1243e26baf1df7205b69824c57afe2c912a37f53
Clem10101998 <57800224+Clem10101998@users.noreply.github.com>
2022-02-21 17:02:00 +0100

    Révisions template-driven forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  //Properties for two way Binding
+  answer = '';
+  defaultQuestion = 'pet';
+  @ViewChild('f') signupForm: NgForm | undefined; //Accès au formulaire
   title = 'formation-angular';
+
+  genders = ['female', 'male'];
+
+  submitted = false;
+
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  onSubmit(form: NgForm){
+    this.submitted = true;
+    console.log(this.signupForm);//Vérification de la soumission du formulaire avec NgForm
+    //Reset form after submission
+    this.signupForm?.reset();
+  }
 }
