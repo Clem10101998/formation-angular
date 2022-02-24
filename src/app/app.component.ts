5909b11dbc5f26877b407eb0585f11212590bf8f
Clem10101998 <clement10101998@gmail.com>
2022-02-24 09:44:56 +0100

    work on polynomes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,44 +7,41 @@ import { FormArray, FormControl, FormGroup, NgForm, Validators } from '@angular/
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit{
+
+  polynome = new FormGroup({
+
+  });
   
-  genders = ['female', 'male'];
-  signupForm!: FormGroup;
-
-  //Définition du formulaire et des validators
   ngOnInit(): void {
-    this.signupForm = new FormGroup({
-      'username': new FormControl(null, Validators.required),
-      //Multiple Validators
-      'email': new FormControl(null, [Validators.required, Validators.email]),
-      'gender': new FormControl('female'),
-    });
-
-    //Permet d'indiquer si le formulaire est valide
-    this.signupForm.statusChanges.subscribe(
-      (status) => console.log(status)
-    );
-
-    //Set the value of each input
-    this.signupForm.setValue({
-      'username': 'Max',
-      'email': 'max@gmail.com',
-      'gender':'male'
-    });
+    //throw new Error('Method not implemented.');
   }
+  
+  a = 0;
+  b = 0;
+  c = 0;
 
-  //Submitting form
-  onSubmit(){
-    console.log(this.signupForm.value);
-    //Reset input after submitting
-    this.signupForm.reset();
+  delta = 0;
+  x1 = 0;
+  x2 = 0;
+
+  getSolution(){
+
+    this.delta = (this.b)**2 - 4*this.a*this.c;
+    if(this.delta == 0){
+
+      this.x1 = -this.b/(2*this.a);
+
+    }else if(this.delta > 0){
+
+      this.x1 = (-this.b - Math.sqrt(this.delta))/(2*this.a);
+      this.x2 = (-this.b + Math.sqrt(this.delta))/(2*this.a);
+    }
+
+    return null;
   }
-
-  //Validator for username
-  showErrorsforUsername(){
-    const username = this.signupForm.controls.username;
-    return username.hasError('required');
-  }
-
 
 }
+function sqrt(delta: number) {
+  throw new Error('Function not implemented.');
+}
+
